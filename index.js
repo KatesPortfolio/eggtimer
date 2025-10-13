81ad5766ae1b08dc4b5b282e4a868df77725a6fa
KatarinaDojcinovska <katarina.dojcinovska98@gmail.com>
2025-10-13 19:02:06 +0200

    function added to the js and changes in html and css

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const sections = document.querySelectorAll(".section");
+const elements = document.querySelectorAll(".element");
 const header = document.querySelector("header");
-const smallEgg = document.getElementById("S-size");
-const mediumEgg = document.getElementById("S-size");
-const largeEgg = document.getElementById("S-size");
+const smallEggBtn = document.getElementById("small-egg-button");
+const mediumEggBtn = document.getElementById("medium-egg-button");
+const largeEggBtn = document.getElementById("large-egg-button");
 const nextButton = document.getElementById("next-button");
+const backButton = document.getElementById("back-button")
 const startButton = document.getElementById("start-button");
 const addEggButton = document.getElementById("add-egg-button");
+const eggContainer = document.querySelectorAll(".egg-selection-container");
+const deleteButton = document.querySelectorAll(".delete-button");
+const sInput = document.getElementById("input-number-s");
+const mInput = document.getElementById("input-number-m");
+const lInput = document.getElementById("input-number-l");
+const sDeleteButton = document.getElementById("s-delete");
+const mDeleteButton = document.getElementById("m-delete");
+const lDeleteButton = document.getElementById("l-delete");
 
+let selectedEgg = null;
+let sEggs = 0;
+let mEggs = 0;
+let lEggs = 0;
 
 window.addEventListener("load", function () {
   hideAll();
@@ -16,6 +30,9 @@ window.addEventListener("load", function () {
     hideAll();
     showSection("egg-selection-section");
     showSection("header-id");
+    hideElement("s-div");
+    hideElement("m-div");
+    hideElement("l-div");
   }, 1500);
 });
 
@@ -27,20 +44,135 @@ function hideAll() {
   }
 }
 
+function hideElement() {
+  for (let element of elements) {
+    if (!element.classList.contains("hidden")) {
+      element.classList.add("hidden");
+    }
+  }
+}
+
 function showSection(sectionId) {
   const section = document.getElementById(sectionId);
 
   section.classList.remove("hidden");
 }
 
+function showElement(elementId) {
+  const element = document.getElementById(elementId);
+  element.classList.remove("hidden");
+}
+
 nextButton.addEventListener("click", function () {
+  if(!sEggs && !mEggs && !lEggs){
+    
+  }
+
+
   hideAll("egg-selection-section");
-  showSection("header-id")
+  showSection("header-id");
   showSection("setTimer-id");
 });
 
+backButton.addEventListener("click", function (){
+  hideAll("setTimer-id");
+  showSection("header-id");
+  showSection("egg-selection-section")
+})
+
 startButton.addEventListener("click", function () {
   hideAll("setTimer-id");
-  showSection("header-id")
+  showSection("header-id");
   showSection("timer-id");
 });
+
+smallEggBtn.addEventListener("click", function () {
+  selectEgg("S");
+});
+mediumEggBtn.addEventListener("click", function () {
+  selectEgg("M");
+});
+largeEggBtn.addEventListener("click", function () {
+  selectEgg("L");
+});
+
+function selectEgg(size) {
+  selectedEgg = size;
+
+  if (size === "S") {
+    smallEggBtn.classList.add("egg-selected");
+  }
+  if (size === "M") {
+    mediumEggBtn.classList.add("egg-selected");
+  }
+  if (size === "L") {
+    largeEggBtn.classList.add("egg-selected");
+  }
+}
+
+function add() {
+  if (selectedEgg === "S") {
+    document.getElementById("s-div").classList.remove("hidden");
+    showElement("s-div");
+  }
+  if (selectedEgg === "M") {
+    document.getElementById("m-div").classList.remove("hidden");
+    showElement("m-div");
+  }
+  if (selectedEgg === "L") {
+    document.getElementById("l-div").classList.remove("hidden");
+    showElement("l-div");
+  }
+}
+
+addEggButton.addEventListener("click", add);
+
+function calculateSmallEgg() {
+  return sEggs * 6;
+}
+
+function calculateMediumEgg() {
+  return mEggs * 7;
+}
+
+function calculateLargeEgg() {
+  return lEggs * 8.5;
+}
+
+function clearInput() {
+  sInput.value = "";
+  mInput.value = "";
+  lInput.value = "";
+
+  sEggs = 0;
+  mEggs = 0;
+  lEggs = 0;
+
+  localStorage.removeItem("sEggs");
+  localStorage.removeItem("mEggs");
+  localStorage.removeItem("lEggs");
+
+  hideElement("s-div");
+  hideElement("m-div");
+  hideElement("l-div");
+}
+
+sDeleteButton.addEventListener("click", clearInput);
+mDeleteButton.addEventListener("click", clearInput);
+lDeleteButton.addEventListener("click", clearInput);
+
+sInput.addEventListener("input", function () {
+  sEggs = parseInt(sInput.value);
+  localStorage.setItem("sEggs", sInput.value);
+});
+
+mInput.addEventListener("input", function () {
+  mEggs = parseInt(mInput.value);
+  localStorage.setItem("mEggs", mInput.value);
+});
+
+lInput.addEventListener("input", function () {
+  lEggs = parseInt(lInput.value);
+  localStorage.setItem("lEggs", lInput.value);
+});
+
