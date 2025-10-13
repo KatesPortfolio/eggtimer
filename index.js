b47fb16543285f19c395087a8077026fc8525ebc
KatarinaDojcinovska <katarina.dojcinovska98@gmail.com>
2025-10-13 09:25:13 +0200

    getting all the buttons into js

---
+++ index.js
@@ -0,0 +1,10 @@
+const sections = document.querySelectorAll(".section");
+const header = document.querySelector("header");
+const smallEgg = document.getElementById("S-size");
+const mediumEgg = document.getElementById("S-size");
+const largeEgg = document.getElementById("S-size");
+const nextButton = document.getElementById("next-button");
+const startButton = document.getElementById("start-button");
+const addEggButton = document.getElementById("add-egg-button");
+
+
